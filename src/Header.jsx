281872bf90ffc731e512aaa7a8b668b78b5ef098
Leonlima01 <Leon_lima01@hotmail.com>
2024-06-30 15:45:43 -0300

    Versão firebase

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ function Header(){
     return(
         <div className="title-div">
             <h1 className="marvel-logo">MARVEL</h1>
-            <h1>Teste de API</h1>
+            <h1>Comic Explorer</h1>
         </div>
     );
 }
